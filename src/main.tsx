364c2a77930d767431c040a68cf8eeab01a327b8
fujikawa721 <ipod.fujikawa@gmail.com>
2024-12-12 23:01:31 +0900

    コンポーネントの呼び出し

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,14 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { App2 } from './App';
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
-const App = () => {
-  return (
-    <>
-      <h1>こんにちは</h1>
-      <p>あああああ</p>
-    </>
-  );
-};
 
 /**画面に出力する構文 */
 root.render(
   <StrictMode>
-    <App />
+    <App2 />
   </StrictMode>
 );
