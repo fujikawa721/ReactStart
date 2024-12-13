9b49056645fb4ad5e5793e0c96a79e768b73d6c6
fujikawa721 <ipod.fujikawa@gmail.com>
2024-12-14 00:46:41 +0900

    Reactのテスト

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
 export const App2 = () => {
+
+    const onClickButton = () =>{
+        alert();
+    };
+
+    const contentStyle = {
+        color : "blue",
+        fontSize:"18px"
+    };
+
+
+    /**{}はJavascript構文を書くよって合図 */
     return (
       <>
-        <h1>こんにちは</h1>
-        <p>あああああ</p>
+        <h1 style={contentStyle}>こんにちは</h1>
+        <p style={contentStyle}>あああああ</p>
+        <button  onClick={onClickButton}>ボタン</button>
       </>
     );
   };
