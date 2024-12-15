750a4f442fa8b9ebc9d5bdf3e1fc2d770caa3a0d
fujikawa721 <ipod.fujikawa@gmail.com>
2024-12-15 23:17:25 +0900

    コンポーネント

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,23 @@ export const App2 = () => {
         alert();
     };
 
-    const contentStyle = {
+    const contentStyleA = {
         color : "blue",
         fontSize:"18px"
     };
+
+    const contentStyleB = {
+      color : "blue",
+      fontSize:"18px"
+  };
 
 
     /**{}はJavascript構文を書くよって合図 */
     return (
       <>
-        <h1 style={contentStyle}>こんにちは</h1>
-        <p style={contentStyle}>あああああ</p>
+        <h1 style={contentStyleA}>こんにちは</h1>
+        <p style={contentStyleA}>あああああ</p>
+        <p style={contentStyleB}>元気です</p>
         <button  onClick={onClickButton}>ボタン</button>
       </>
     );
--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { App2 } from './App';
 
-const rootElement = document.getElementById('root');
+const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
 
